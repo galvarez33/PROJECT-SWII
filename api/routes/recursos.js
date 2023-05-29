9396e4461560a3c55f8fb841cc213061b9cc9571
David Gilarranz Ortega <d.gilarranz1@usp.ceu.es>
2023-05-29 20:51:02 +0200

    Drop recurso elimina colección

--- api/routes/recursos.js
+++ api/routes/recursos.js
@@ -104,6 +104,9 @@ router.delete('/:idRecurso', async function (req, res, next) {
 
     // 3. Devolver respuesta
     if (mongoResponse.deletedCount) {
+      // Borrar la colección asociada al recurso
+      await db.dropCollection(req.params.idRecurso);
+
       sendResponse(res, 200, "Todo correcto");
     } else {
       sendResponse(res, 404, `El recurso ${req.params.idRecurso} no existe`);
@@ -170,6 +173,49 @@ async function getContent(req, res, validator) {
   //console.log(data)
 
 }
+
+/** GET: Method to get list of resources  */
+router.get('/:idRecurso', async function (req, res, next) {
+  // Comprobamos admin
+  const admin = checkAdmin(req, res);
+  if (admin) {
+    // 1. Obtenemos página y offset
+    const page = Math.max(1, req.query.page);             // La página debe ser mayor que 1, para que cuando se calcule el offset sea mayor o igual que 0
+    const page_size = Math.min(req.params.page_size, process.env.MAX_PAGE_SIZE);  // El tamaño de página debe ser menor o igual que MAX_PAGE_SIZE
+    const offset = (page - 1) * page_size;                // El se calcula como (página - 1) * tamaño_página, para que la página 1 se corresponda a los
+                                                          // primeros resultados [0, n)
+
+    // 1. Consulta a mongo -> Todos los activos asociados a un recurso (paginados)
+    const resource = req.params.idRecurso;
+    const databaseManager = Database.getInstance();
+    const db = databaseManager.client.db("scrapiffy");
+    const mongoResponse = await db.collection(resource)
+      .find()
+      .limit(page_size)
+      .skip(offset)
+
+    if (mongoResponse) {
+      // Si recurso existe, da formato a la respuesta
+      const activos = [];
+      const ocurrencias = [];
+      mongoResponse.ocurrencias.forEach(o => {
+        ocurrencias.push({
+          timestamp: o.timestamp,
+          source: o.source
+        });
+      });
+
+      const response = {
+        activos,
+        next: `/recursos/${resource}?${page + 1}&${page_size}`
+      };
+      sendResponse(res, 200, response);
+    } else {
+      // Si el recurso no existe, devuelve error
+      sendResponse(res, 404, `El recurso ${asset} no existe`);
+    }
+  }
+});
 
 router.get('/:idRecurso/:idActivo', async function (req, res, next) {
   // 1. Consulta a mongo -> activos
