3347b96901923a2abeb2c597799ebe4b6c97ec2e
GOnzaloAlvarez <g.alvarez33@usp.ceu.es>
2023-05-30 20:43:16 +0200

    funcionalidad de recursos

--- cliente/public/sendRequest.js
+++ cliente/public/sendRequest.js
@@ -2,12 +2,19 @@ async function senderRequest() {
     try {
         const elemento = document.getElementById("search-input").value;
         let recurso = document.getElementById("recursos").value;
+        if (recurso == "default"){
+            alert("Selccione un recurso")
+            return
+        }
         let response = await axios("http://localhost:3000/recursos/" + recurso + "/" + elemento)
         const res = document.getElementById('res');
-        res.innerHTML = JSON.stringify(response.data);
-    } catch {
-        res.innerHTML = ("No se ha encontrado ninguna ocurrencia")
 
-    }
+        if (response.data) {
+            document.getElementById("res").style.color = "#a82c2c";
+            res.innerHTML = JSON.stringify(response.data);
+        }
+    } catch {
+        res.innerHTML = ("ENHORABUENA, No se ha encontrado ninguna ocurrencia.")
+    };
 }
 
