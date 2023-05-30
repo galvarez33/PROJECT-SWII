2e752ed1b81d4fa75e22e0dc86afda3b2486e972
PedroB <bereilhp@gmail.com>
2023-05-30 19:29:24 +0200

    Cliente puede hacer consultas a la api y ver respuesta

--- api/app.js
+++ api/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors')
 
 const session = require('express-session');
 
@@ -24,6 +25,8 @@ app.use(session({
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(cors())
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -40,15 +43,23 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
+app.get('/:idRecurso/:idActivo', function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
+
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
+  res.setHeader('Access-Control-Allow-Origin', '*'); 
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+  next()
 });
 
 module.exports = app;
--- cliente/public/sendRequest.js
+++ cliente/public/sendRequest.js
@@ -1,7 +1,9 @@
-
-
 async function  senderRequest(){
     const elemento = document.getElementById("search-input").value;
     let response = await axios("http://localhost:3000/recursos/emails/" + elemento);
-    console.log(response)
+
+    const res = document.getElementById('res');
+
+    res.innerHTML = JSON.stringify(response.data);
 }
+
