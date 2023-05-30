c3e15f1b3c2dcf39aec848168de1b3c5218b5841
GOnzaloAlvarez <g.alvarez33@usp.ceu.es>
2023-05-30 20:11:15 +0200

    select de recursos

--- cliente/public/sendRequest.js
+++ cliente/public/sendRequest.js
@@ -1,9 +1,13 @@
-async function  senderRequest(){
-    const elemento = document.getElementById("search-input").value;
-    let response = await axios("http://localhost:3000/recursos/emails/" + elemento);
+async function senderRequest() {
+    try {
+        const elemento = document.getElementById("search-input").value;
+        let recurso = document.getElementById("recursos").value;
+        let response = await axios("http://localhost:3000/recursos/" + recurso + "/" + elemento)
+        const res = document.getElementById('res');
+        res.innerHTML = JSON.stringify(response.data);
+    } catch {
+        res.innerHTML = ("No se ha encontrado ninguna ocurrencia")
 
-    const res = document.getElementById('res');
-
-    res.innerHTML = JSON.stringify(response.data);
+    }
 }
 
